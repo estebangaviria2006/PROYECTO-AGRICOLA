4bab576d03f7993add1fecd800669e45694ff33d
estebangaviria2006 <munozleandro343@gmail.com>
2023-08-16 23:08:16 -0500

    add fuentes

--- JOSE C/TRABAJOnJSnLPPnPHPnPYTHON___9064b6ef592a541___/candidatos/2.js
+++ JOSE C/TRABAJOnJSnLPPnPHPnPYTHON___9064b6ef592a541___/candidatos/2.js
@@ -10,14 +10,21 @@ const resultadoHuawei = document.getElementById("resultadoHuawei");
 let cantidadSamsung = 0;
 let cantidadMotorola = 0;
 let cantidadHuawei = 0;
-
+           /* Registra un evento a un objeto en específico */
 formulario.addEventListener("submit", (event) => {
+       /* cancela el evento, lo que significa que cualquier acción por defecto que deba producirse como resultado de este evento, no ocurrirá */
   event.preventDefault();
-
+                            
   const marcaSeleccionada = parseInt(document.getElementById("marca").value);
+                            /* La función parseInt comprueba el primer argumento, una cadena, e intenta devolver un entero de la base especificada */
   const cantidadVendida = parseInt(document.getElementById("cantidad").value);
 
+  /* evalúa una expresión, comparando el valor de esa expresión con una instancia case , 
+  y ejecuta declaraciones asociadas a ese case , así como las declaraciones en los case que siguen */
   switch (marcaSeleccionada) {
+
+    /* una instancia case es usada para ser comparada con la expresión . Si la expresión coincide con el valor, 
+    las declaraciones dentro de la instancia case se ejecutan hasta que se encuentre el final de la declaración una interrupción break . */
     case 1:
       cantidadSamsung += cantidadVendida;
       break;
@@ -36,12 +43,15 @@ formulario.addEventListener("submit", (event) => {
   resultado_Samsung.textContent = `Samsung: Cantidad: ${cantidadSamsung}`; 
   resultado_Motorola.textContent = `Motorola: Cantidad: ${cantidadMotorola}`; 
   resultado_Huawei.textContent = `Huawei: Cantidad: ${cantidadHuawei}`; 
- 
+                   /* representa el contenido de texto de un nodo y sus dencendientes */
+                          /* nodo son elementos que son hijos directos, es decir sus descendientes inmediatos */
+                            /* dencendientes son todos los elementos anidados de un elemento dado, incluyendo los hijos, sus hijos y así sucesivamente. */
 
   resultadoSamsung.textContent = `Valor Total: $${TotalSamsung}`; 
   resultadoMotorola.textContent = `Valor Total: $${TotalMotorola}`; 
   resultadoHuawei.textContent = `Valor Total: $${TotalHuawei}`; 
 
+              /* restaura los elementos de un formulario a sus valores por defecto */
   formulario.reset();
 });
 
--- JOSE C/TRABAJOnJSnLPPnPHPnPYTHON___9064b6ef592a541___/candidatos/votos.php
+++ JOSE C/TRABAJOnJSnLPPnPHPnPYTHON___9064b6ef592a541___/candidatos/votos.php
@@ -1,94 +1,78 @@
-<form action="#" method="POST">
-    <select name="candidato" id="candidato">
-        <option value="">Seleccionar...</option>
-        <option value="0">Sergio Mauricio Zuñiga</option>
-        <option value="1">Yider Luna Joven</option>
-        <option value="2">Franklin Alexander Vega</option>
-        <option value="3">Voto en blanco</option>
-    </select>
-    <button type="submit">Votar</button>
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sistema de Votación</title>
+</head>
+<body>
+    <h1>Elecciones </h1>
+    <form method="post">
+        <label for="candidate">Selecciona un candidato:</label>
+        <select name="candidate" id="candidate">
+            <option value="#">Selecciona un candidato...</option>
+            <option value="1">Sergio Mauricio Zúñiga</option>
+            <option value="2">Yider Luna Joven</option>
+            <option value="3">Franklin Alexander Vega</option>
+            <option value="4">Voto en blanco</option>
+        </select>
+        <button type="submit" name="vote">Votar</button>
+        <button type="submit" name="reset">Borrar Votos</button>
+    </form>
 
-<form action="#" method="POST">
-    <input type="hidden" name="reiniciarVotos">
-    <button type="submit">Reiniciar votos</button>
-</form>
-
-<?php
-
-session_start();   /* crea una sesión o reanuda la actual basada en un identificador de sesión pasado */
-if (isset($_POST['candidato'])) {  /* se utiliza para verificar si una variable está definida y su valor no es NULL */
-    $candidato = intval($_POST['candidato']); /* recibe un argumento de cualquier tipo y devuelve su representación como entero */
-    if (!isset($_SESSION['candidatos'])) {
-        $_SESSION['candidatos'] = [0, 0, 0, 0];      /* es un array especial utilizado para guardar información a 
-                                                     través de los requests que un usuario hace durante su visita a un sitio web o aplicación. */
-    }
-    $_SESSION['candidatos'][$candidato] += 1;
-    var_dump($_SESSION['candidatos']);
-
-    $candidato1 = $_SESSION['candidatos'][0];
-    $candidato2 = $_SESSION['candidatos'][1];
-    $candidato3 = $_SESSION['candidatos'][2];
-    $candidato4 = $_SESSION['candidatos'][3];
     
-    if ($candidato1 == $candidato2 && $candidato2 == $candidato3 && $candidato1 == $candidato3 && $candidato2 == $candidato4 && $candidato3 == $candidato4 && $candidato1 == $candidato4) {
-        echo "Todos los candidatos llevan la misma cantidad de votos";
-        return;
-    }elseif ($candidato1 > $candidato2 && $candidato1 > $candidato3 && $candidato1 > $candidato4) {
-        echo " El candidato:  Sergio Mauricio Zuñiga  posee la mayor cantidad de votos";
-        return;
-    }elseif ($candidato2 > $candidato1 && $candidato2 > $candidato3 && $candidato2 > $candidato4) {
-        echo " El candidato:  Yider Luna Joven  posee la mayor cantidad de votos";
-        return;
-    }elseif ($candidato3 > $candidato1 && $candidato3 > $candidato2 && $candidato3 > $candidato4) {
-        echo " El candidato:  Franklin Alexander Vega  posee la mayor cantidad de votos";
-        return;
-    }elseif ($candidato4 > $candidato1 && $candidato4 > $candidato2 && $candidato4 > $candidato3) {
-        echo " Los Votos en blanco  posee la mayor cantidad de votos";
-        return;
-    }elseif ($candidato1 == $candidato2 && $candidato1 > $candidato3 && $candidato1 > $candidato4) {
-        echo " Los candidatos:  Sergio Mauricio Zuñiga y  Yider Luna Joven  poseen los mismos votos";
-        return;
-    }elseif ($candidato2 == $candidato3 && $candidato2 > $candidato1 && $candidato2 > $candidato4) {
-        echo " Los candidatos:  Yider Luna Joven y Franklin Alexander Vega  poseen los mismos votos";
-        return;
-    }elseif ($candidato1 == $candidato3 && $candidato1 > $candidato2 && $candidato1 > $candidato4) {
-        echo " Los candidatos:  Sergio Mauricio Zuñiga  y Franklin Alexander Vega  poseen los mismos votos";
-        return;
-    }elseif ($candidato1 == $candidato4 && $candidato1 > $candidato2 && $candidato1 > $candidato3) {
-        echo " Los candidatos:  Sergio Mauricio Zuñiga y Votos en blanco  poseen los mismos votos";
-        return;
-    }elseif ($candidato2 == $candidato4 && $candidato2 > $candidato1 && $candidato2 > $candidato3) {
-        echo " Los candidatos:  Yiber Luna Joven y Votos en blanco  poseen los mismos votos";
-        return;
-    }elseif ($candidato3 == $candidato4 && $candidato3 > $candidato1 && $candidato3 > $candidato2) {
-        echo " Los candidatos:  Franklin Alexander Vega y Votos en blanco  poseen los mismos votos";
-        return;
-    }elseif ($candidato1 == $candidato2 && $candidato2 == $candidato3 && $candidato1 == $candidato3) {
-        echo " Los candidatos:  Sergio Mauricio Zuñiga,  Yider Luna Joven y Franklin Alexander Vega  poseen los mismos votos";
-        return;
-    }elseif ($candidato1 == $candidato2 && $candidato2 == $candidato4 && $candidato1 == $candidato4) {
-        echo " Los candidatos:  Sergio Mauricio Zuñiga,  Yider Luna Joven y Votos en blanco  posseen la misma cantidad de votos";
-        return;
-    }elseif ($candidato1 == $candidato3 && $candidato3 == $candidato4 && $candidato1 == $candidato4) {
-        echo " Los candidatos:  Sergio Mauricio Zuñiga,  Franklin Alexander Vega y Votos en blanco  posseen la misma cantidad de votos";
-        return;
-    }elseif ($candidato2 == $candidato3 && $candidato3 == $candidato4 && $candidato2 == $candidato4) {
-        echo " Los candidatos:  Yider Luna Joven,  Franklin Alexander y Vega  Votos en blanco  posseen la misma cantidad de Votos";
-        return;
+    <?php
+    /* crea una sesión o reanuda la actual basada en un identificador de sesión pasado */
+    session_start();
+
+        /* se utiliza para verificar si una variable está definida y su valor no es NULL */
+    if (!isset($_SESSION['votes'])) {
+        $_SESSION['votes'] = array(1 => 0, 2 => 0, 3 => 0, 4 => 0);
     }
-}
-?>
+              /* es una variable superglobal que se utiliza para recopilar datos de un formulario HTML y enviarlos a un script de PHP */
+    if (isset($_POST['vote'])) {
+        $selectedCandidate = $_POST['candidate'];
 
-<?php
-      /* se utiliza para verificar si una variable está definida y su valor no es NULL */
-if (isset($_POST['reiniciarVotos'])) {
-    // Vuelve a colocar los votos en 0.
-    $_SESSION['candidatos'] = [0, 0, 0, 0];
-    /* es un array especial utilizado para guardar información a 
-     través de los requests que un usuario hace durante su visita a un sitio web o aplicación. */
-    // O
-    echo "Votos reiniciados";
-}
+        if ($selectedCandidate == "#") {
+            echo "<p style='color: black;'>Error: No has seleccionado ningún candidato.</p>";
+        } else {
+             /* es un array especial utilizado para guardar información a 
+            través de los requests que un usuario hace durante su visita a un sitio web o aplicación. */
+            $_SESSION['votes'][$selectedCandidate]++;
+        }
+    }
+        /* se utiliza para verificar si una variable está definida y su valor no es NULL */
+    if (isset($_POST['reset'])) {
+        $_SESSION['votes'] = array(1 => 0, 2 => 0, 3 => 0, 4 => 0);
+    }
 
-?>
+    echo '<h2>Resultados:</h2>';
+
+    /* es un tipo especial de bucle que permite recorrer estructuras que contienen varios elementos 
+    (como matrices, recursos u objetos) sin necesidad de preocuparse por el número de elementos */
+    foreach ($_SESSION['votes'] as $candidateId => $votes) {
+        echo "Candidato $candidateId: $votes votos<br>";
+    }
+                /* devuelve el valor del parámetro considerado "el mayor" según las reglas de comparación estándar */
+    $maxVotes = max($_SESSION['votes']);
+
+            /* devuelve las claves, numéricas y de tipo string, del array  */
+    $winners = array_keys($_SESSION['votes'], $maxVotes);
+
+       /* devuelve el número de elementos que hay en el array */
+    if (count($winners) === 1) {
+        $winner = $winners[0];
+        echo "<h2>Ganador: Candidato $winner</h2>";
+    } elseif ($maxVotes === 0) {
+        echo "<h2>No se ha registrado ningún voto.</h2>";
+    } else {
+                                                   /* Devuelve un string que contiene la representación de todos los elementos del array en el mismo orden */
+        echo "<h2>Empate entre los candidatos: " . implode(', ', $winners) . "</h2>";
+    }
+                   /* devuelve la suma de los valores de un array */
+    $totalVotes = array_sum($_SESSION['votes']);
+    echo "<h2>Total de votos: $totalVotes</h2>";
+    ?>
+
+
+
+</body>
+</html>
